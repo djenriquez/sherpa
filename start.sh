eeac910bd65d2ad45f7d0863b8fe2d165e03f801
DJ Enriquez <dj.enriquez@infospace.com>
2016-10-14 12:12:48 -0700

    Renaming proxy.conf to nginx.conf

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/sbin/nginx -c /etc/nginx/proxy.conf -t && exec /usr/sbin/nginx -c /etc/nginx/proxy.conf -g "daemon off;"
+/usr/sbin/nginx -c /etc/nginx/nginx.conf -t && exec /usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
